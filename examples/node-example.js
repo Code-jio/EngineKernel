d435d1a534c53e817e28e939aba279a1d7c466b1
hubigui <coder_hbg@163.com>
2025-04-21 14:41:43 +0800

    添加使用示例

--- examples/node-example.js
+++ examples/node-example.js
@@ -1,6 +1,3 @@
-// 安装依赖：npm install ../dist/my-library.min.js
-const MyLibrary = require('../dist/my-library.min.js');
+const MyLibrary = require('../dist/engine-kernel.min.js');
 
-// 使用库功能示例
 console.log('Library version:', MyLibrary.version);
-// 实际调用库方法的代码示例
