ceef4fba45fd1287c7ec4b1618027ee49341e252
hubigui <coder_hbg@163.com>
2025-04-21 16:55:11 +0800

    天空盒、html标签转化插件占位

---
+++ src/plugins/webgl/css2DRender.ts
@@ -0,0 +1,3 @@
+// html标签转object3D
+import * as THREE from "three"
+import { CSS2DRenderer } from 'three/examples/jsm/renderers/CSS2DRenderer';
---
+++ src/plugins/webgl/css3DRender.ts
@@ -0,0 +1,3 @@
+// html标签转object3D
+import * as THREE from "three"
+import { CSS3DRenderer } from 'three/examples/jsm/renderers/CSS3DRenderer';
