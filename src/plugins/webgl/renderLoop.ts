a78ea25840fcde22e3da6bf31b5c3b42c075f259
Code-jio <1054037036@qq.com>
2025-04-21 23:46:49 +0800

    refactor(renderLoop): 重构渲染循环逻辑以提高代码可维护性

--- src/plugins/webgl/renderLoop.ts
+++ src/plugins/webgl/renderLoop.ts
@@ -3,27 +3,28 @@ import { Clock } from "three"
 import * as THREE from "three"
 import eventBus from "eventBus/eventBus"
 
-export default class renderLoop extends BasePlugin {
+export default class RenderLoop extends BasePlugin {
     private clock: THREE.Clock
-    private taskList: []
+    private taskList: (() => void)[] = []
     constructor(meta: any) {
         super(meta)
-        this.clock = new THREE.Clock()
         this.taskList = []
-    }
-
-    render() {
-        this.taskList.forEach((callback: () => void) => {
-            eventBus.on("update", callback)
-
-            callback()
-        })
-        return
+        this.clock = new THREE.Clock()
     }
 
     initialize() {
-        requestAnimationFrame(() => {
-            this.render()
-        })
+        const render = () => {
+            eventBus.emit("update") // 触发更新事件
+            // 执行任务
+            this.taskList.forEach((task) => { 
+                task()
+            })
+            requestAnimationFrame(render); // 需要持续循环
+        };
+        requestAnimationFrame(render);
+    }
+
+    addTask(callback: () => void) {
+        this.taskList.push(callback);
     }
 }
