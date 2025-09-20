25f6d2b9b86abab4517997370990910d8c2018ff
code_jio <1054037036@qq.com>
2025-09-20 14:20:00 +0800

    refactor(plugins): 重构插件导出路径，将特效相关插件移至effects目录
    将粒子系统、烟雾、火焰等特效相关插件从webgl目录移动到effects目录，使项目结构更清晰。同时移除不再使用的CameraPlugin导出。

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -3,14 +3,13 @@ export { OrbitControls } from '../utils/three-imports';
 
 export { BaseScene } from './webgl/baseScene';
 export { AnimationControls } from './webgl/animationControls';
-export { CameraPlugin } from './webgl/cameraPlugin';
 export { GLMonitor } from './webgl/glMonitor';
 export { LODPlugin } from './webgl/lod';
 export { BaseControls } from './webgl/baseControl';
 export { Performance } from './webgl/performance';
 export { RenderLoop } from './webgl/renderLoop';
 export { ResourceReaderPlugin } from './webgl/resourceReaderPlugin';
-export { TaskPriority, TaskStatus, TaskScheduler } from './webgl/asyncTaskScheduler';
+export { TaskPriority, TaskStatus, TaskScheduler } from '../tools/asyncTaskScheduler';
 export { SkyBox, SkyBoxType} from './webgl/skyBox';
 export { WebGLContextLose } from './webgl/webglContextLose';
 export { CSS3DRenderPlugin } from './webgl/css3DRender';
@@ -19,18 +18,18 @@ export { LayerManager } from './webgl/layerManager';
 export { ModelMarker } from './webgl/3DModelMarker';
 export { MousePickPlugin } from './webgl/mousePickPlugin';
 export { TextMarkerPlugin } from "./webgl/TextMarkerPlugin";
-export { ParticleEmitter } from "./webgl/ParticleEmitter" 
+export { ParticleEmitter } from "./effects/ParticleEmitter" 
 
 export { BuildingControlPlugin } from './webgl/BuildingControlPlugin';
 
 export { WaterMarker } from './webgl/waterMarker';
 export { WaterMarkerPlugin } from './webgl/WaterMakerPlugin';
-export { FireParticleSystem, FireEffectManager } from './webgl/fireMarker';
-export { CloudMarkerPlugin } from './webgl/cloudMarkerPlugin';
-export { SmokeEffectManager, SmokeParticleSystem } from "./webgl/SmokeMarker";
+export { FireParticleSystem, FireEffectManager } from './effects/fireMarker';
+export { CloudMarkerPlugin } from './effects/cloudMarkerPlugin';
+export { SmokeEffectManager, SmokeParticleSystem } from "./effects/SmokeMarker";
 
-export { FountainParticleSystem } from "./webgl/FountainMarker"
-export { SparkParticleSystem } from "./webgl/SparkMarker"
+export { FountainParticleSystem } from "./effects/FountainMarker"
+export { SparkParticleSystem } from "./effects/SparkMarker"
 
 export { EffectComposer, RenderPass, OutlinePass, ShaderPass, FXAAShader, OutputPass } from '../utils/three-imports';
 export { CSS2DRenderer, CSS2DObject, CSS3DRenderer, CSS3DObject } from '../utils/three-imports';
@@ -39,6 +38,7 @@ export { OutLinePlugin } from "./webgl/OutLinePlugin"
 export { DataRainEffect } from "./effects/DataRainEffect"
 export { TechRingEffect } from "./effects/TechRingEffect"
 export { PostProcessingPlugin } from "./postProcessing/index"
+export { StaticGeometryMerger } from "./merge/index"
 
 
 // export { BaseModel } from './webgl/baseModel';
